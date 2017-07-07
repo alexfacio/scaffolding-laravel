54a7c500574f280db34c4c106738a80fd5e98910
Alejandro Facio <a.facio@unexpect.mx>
2017-07-07 16:58:07 -0500

    Modificando nombre.

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'SINDI: The Friedman Group',
+    'name' => 'Project Number ONE',
 
     /*
     |--------------------------------------------------------------------------
